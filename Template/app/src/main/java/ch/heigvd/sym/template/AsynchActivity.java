1bc89ee0e63ffdc0b3c7869843bf774fc52234e8
Ludwig <loic.frueh@gmail.com>
2018-11-23 16:48:32 +0100

    Navigation Ok now

--- Template/app/src/main/java/ch/heigvd/sym/template/AsynchActivity.java
+++ Template/app/src/main/java/ch/heigvd/sym/template/AsynchActivity.java
@@ -2,13 +2,12 @@ package ch.heigvd.sym.template;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import org.json.JSONObject;
 
-public class AsynchActivity extends Activity {
-    private TextView data;
-    private TextView result;
+public class AsynchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
