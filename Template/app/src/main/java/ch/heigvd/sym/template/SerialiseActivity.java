1f1759088421950c01a1e759fa4abee1ef9e9ffa
Dejvid Muaremi <dejvid95@hotmail.com>
2018-11-23 16:00:14 +0100

    update serialisable

--- Template/app/src/main/java/ch/heigvd/sym/template/SerialiseActivity.java
+++ Template/app/src/main/java/ch/heigvd/sym/template/SerialiseActivity.java
@@ -7,8 +7,55 @@ import android.widget.TextView;
 import org.json.JSONObject;
 
 public class SerialiseActivity extends Activity {
-    private TextView data;
-    private TextView result;
+    /*
+    <TextView
+            android:id="@+id/label_serializable_first_name"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:layout_marginTop="50dp"
+            android:text="string/serializable_first_name" />
+
+        <EditText
+            android:id="@+id/edit_serializable_first_name"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:text="serializable_first_name" />
+
+        <TextView
+            android:id="@+id/label_serializable_last_name"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:layout_marginTop="20dp"
+            android:text="string/serializable_last_name" />
+
+        <EditText
+            android:id="@+id/edit_serializable_last_name"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:text="serializable_lastname" />
+
+        <Button
+            android:id="@+id/sendRequest"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:text="string/serializable_send" />
+
+        <TextView
+            android:id="@+id/response_serializable_first_name"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:layout_marginTop="20dp"
+            android:text="string/waiting" />
+
+        <TextView
+            android:id="@+id/response_serializable_last_name"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:text="string/waiting" />
+    */
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
