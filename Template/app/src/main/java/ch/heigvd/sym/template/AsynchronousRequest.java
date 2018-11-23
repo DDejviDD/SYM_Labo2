65502f51acecc1b419a32ff9440571aee1939145
zedsdead95 <mr.walid.koubaa@gmail.com>
2018-11-23 20:51:22 +0100

    Added Asynchronous request with OkHttp3

---
+++ Template/app/src/main/java/ch/heigvd/sym/template/AsynchronousRequest.java
@@ -0,0 +1,52 @@
+package ch.heigvd.sym.template;
+
+import java.io.IOException;
+import java.util.EventListener;
+import java.util.LinkedList;
+import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.RequestBody;
+import okhttp3.OkHttpClient;
+
+
+// Source: https://www.youtube.com/watch?v=oGWJ8xD2W6k
+//         http://square.github.io/okhttp/
+
+
+public class AsynchronousRequest {
+
+    private static final MediaType  JSON_MEDIA
+            = MediaType.parse("application/json; charset=utf-8");
+
+    private static final MediaType TEXT_MEDIA
+            = MediaType.parse("text/plain; charset=utf-8");
+
+
+    private OkHttpClient client = new OkHttpClient();
+
+    //private List<EventListener> listeners = new LinkedList<>();
+
+    // Run a new thread for sending the request, so that activity process will not be interrupted
+    public void sendRequest(final String request, final String myUrl, final MediaType mediaType) {
+        new Thread() {
+            public void run() {
+                RequestBody body = RequestBody.create(mediaType, request);
+                Request SendingRequest = new Request.Builder().url(myUrl).post(body).build();
+                try {
+                    Response response = client.newCall(SendingRequest).execute();
+
+                    /*for (EventListener event : listeners) {
+                        if (event.handleServerResponse(response.body().string())) break;
+                    }*/
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+}
