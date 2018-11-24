9391afd36cf4691d2e85a6cd47ecf4a30f3a33e3
Ludwig <loic.frueh@gmail.com>
2018-11-24 18:44:31 +0100

    Begin graphQL but nothing displayed T.T

--- Template/app/src/main/java/ch/heigvd/sym/template/GraphActivity.java
+++ Template/app/src/main/java/ch/heigvd/sym/template/GraphActivity.java
@@ -1,20 +1,78 @@
 package ch.heigvd.sym.template;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ScrollView;
+import android.widget.Spinner;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.MediaType;
+
 public class GraphActivity extends AppCompatActivity {
-    private TextView data;
-    private TextView result;
+
+    // GUI elements
+    Spinner authorsSpinner = null;
+    ScrollView postView = null;
+
+
+
+    ArrayAdapter<String> authorsAdapterSpinner;
+    List<String> authorsList = new ArrayList<>();
+    Context savedContext = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.graph);
 
-    }
+        // Link the GUI element with view
+        authorsSpinner = findViewById(R.id.authorsSpinner);
+        postView = findViewById(R.id.postView);
 
+        // Request to fill the Spinner options
+        AsynchronousRequest request = new AsynchronousRequest();
+        request.newListener(
+                new RequestListener() {
+                    @Override
+                    public boolean handlerForRequest(final String response) {
+                        try {
+                            JSONArray authorsJSON = new JSONObject(response).getJSONArray("allAuthors");
+                            for (int i = 0; i < authorsJSON.length(); ++i) {
+                                authorsList.add(authorsJSON.getJSONObject(i).getString("first_name") + " "
+                                        + authorsJSON.getJSONObject(i).getString("last_name"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            return false;
+                        }
+
+                        runOnUiThread(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        authorsAdapterSpinner.clear();
+                                        authorsAdapterSpinner.addAll(authorsList);
+                                        authorsAdapterSpinner.notifyDataSetChanged();
+                                    }
+                                }
+                        );
+
+                        return true;
+                    }
+                }
+        );
+        request.postRequest("{ \"query\": \"{allAuthors{id, first_name, last_name }}\" }", "http://sym.iict.ch/api/graphql", MediaType.parse("application/json; charset=utf-8"));
+        authorsList.add("Loading...");
+        authorsAdapterSpinner = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, authorsList);
+        authorsAdapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        authorsSpinner.setAdapter(authorsAdapterSpinner);
+    }
 }
