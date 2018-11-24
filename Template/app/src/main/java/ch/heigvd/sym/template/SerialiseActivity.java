7c47ad4388fe1a87bf2386fb1f9ef6eb156257b7
Dejvid Muaremi <dejvid95@hotmail.com>
2018-11-24 21:09:44 +0100

    xml in progress. may need a sync

--- Template/app/src/main/java/ch/heigvd/sym/template/SerialiseActivity.java
+++ Template/app/src/main/java/ch/heigvd/sym/template/SerialiseActivity.java
@@ -2,6 +2,7 @@ package ch.heigvd.sym.template;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Xml;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,6 +10,10 @@ import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.IOException;
+import java.io.StringWriter;
 
 import okhttp3.MediaType;
 
@@ -18,7 +23,8 @@ public class SerialiseActivity extends AppCompatActivity {
     private EditText edit_serializable_first_name;
     private TextView label_serializable_last_name;
     private EditText edit_serializable_last_name;
-    private Button   serializable_send_request;
+    private Button   serializable_send_xml_request;
+    private Button   serializable_send_json_request;
     private TextView response_serializable_type;
     private TextView response_serializable_first_name;
     private TextView response_serializable_last_name;
@@ -32,12 +38,19 @@ public class SerialiseActivity extends AppCompatActivity {
         edit_serializable_first_name = findViewById(R.id.edit_serializable_first_name);
         label_serializable_last_name = findViewById(R.id.label_serializable_last_name);
         edit_serializable_last_name = findViewById(R.id.edit_serializable_last_name);
-        serializable_send_request = findViewById(R.id.serializable_send_request);
+        serializable_send_xml_request = findViewById(R.id.serializable_send_xml_request);
+        serializable_send_json_request = findViewById(R.id.serializable_send_json_request);
         response_serializable_type = findViewById(R.id.response_serializable_type);
         response_serializable_first_name = findViewById(R.id.response_serializable_first_name);
         response_serializable_last_name = findViewById(R.id.response_serializable_last_name);
 
-        serializable_send_request.setOnClickListener(new View.OnClickListener() {
+        serializable_send_xml_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xmlSender();
+            }
+        });
+        serializable_send_json_request.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 jsonSender();
@@ -45,16 +58,66 @@ public class SerialiseActivity extends AppCompatActivity {
         });
 
     }
-    public void jsonSender(){
-        JSONObject data = new JSONObject();
+
+    public void xmlSender() {
+        XmlSerializer xmlData = Xml.newSerializer();
+        StringWriter xml = new StringWriter();
         try {
-            data.put("type", "json");
-            data.put(label_serializable_first_name.getText().toString(), edit_serializable_first_name.getText().toString());
-            data.put(label_serializable_last_name.getText().toString(), edit_serializable_last_name.getText().toString());
+            xmlData.setOutput(xml);
+            xmlData.startDocument("UTF-8", null);
+            xmlData.docdecl(" directory SYSTEM \"http://sym.iict.ch/directory.dtd\"");
+
+            xmlData.startTag("","directory");
+            xmlData.startTag("","type");
+            xmlData.text("xml");
+            xmlData.endTag("","type");
+
+            xmlData.startTag("",label_serializable_first_name.getText().toString());
+            xmlData.text(edit_serializable_first_name.getText().toString());
+            xmlData.endTag("",label_serializable_first_name.getText().toString());
+
+            xmlData.startTag("",label_serializable_last_name.getText().toString());
+            xmlData.text(edit_serializable_last_name.getText().toString());
+            xmlData.endTag("",label_serializable_last_name.getText().toString());
+
+            xmlData.endTag("", "directory");
+            xmlData.endDocument();
+            sendXMLRequest(xml.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void sendXMLRequest(String request) {
+        AsynchronousRequest aRequest = new AsynchronousRequest();
+        aRequest.newListener(new RequestListener() {
+            @Override
+            public boolean handlerForRequest(final String response) {
+                final String r = response;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        response_serializable_first_name.setText(r);
+                    }
+                });
+                return true;
+            }
+        });
+        aRequest.postRequest(request, "http://sym.iict.ch/rest/xml", MediaType.parse("application/xml; charset=utf-8"));
+    }
+
+    public void jsonSender(){
+        JSONObject jsonData = new JSONObject();
+        try {
+            jsonData.put("type", "json");
+            jsonData.put(label_serializable_first_name.getText().toString(), edit_serializable_first_name.getText().toString());
+            jsonData.put(label_serializable_last_name.getText().toString(), edit_serializable_last_name.getText().toString());
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        sendJsonRequest(data.toString());
+        sendJsonRequest(jsonData.toString());
     }
     public void sendJsonRequest(String request) {
         AsynchronousRequest aRequest = new AsynchronousRequest();
