55b84657de926719b63dde69b6b20c62bdb0778f
Dejvid Muaremi <dejvid95@hotmail.com>
2018-11-25 17:55:44 +0100

    does'nt work but does'nt crash...

--- Template/app/src/main/java/ch/heigvd/sym/template/DelayedActivity.java
+++ Template/app/src/main/java/ch/heigvd/sym/template/DelayedActivity.java
@@ -1,19 +1,17 @@
 package ch.heigvd.sym.template;
 
-import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.json.JSONObject;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 import java.util.zip.DataFormatException;
 
@@ -23,33 +21,39 @@ public class DelayedActivity extends AppCompatActivity {
     private EditText edit_delayed_data;
     private Button delayed_send_request;
     private TextView delayed_result;
+    private Handler handler = new Handler();
 
-    private List<String> pool;
+    private final int DELAY = 10000;
 
+    private ArrayList<String> pool = new ArrayList<>();
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+                if (isOnline()&&!pool.isEmpty()) {
+                    while (!pool.isEmpty()){
+                        if(isOnline()){
+                            sendRequest(pool.get(0));
+                            pool.remove(0);
+                        }
+                        else {
+                            handler.postDelayed(runnable, DELAY);
+                        }
+                    }
+                } else {
+                    handler.postDelayed(runnable, DELAY);
+                }
+            }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.delayed);
 
-        pool = Collections.synchronizedList(new ArrayList<String>());
+        new Thread(runnable).start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (!Thread.currentThread().isInterrupted()){
-                    while (!pool.isEmpty()){
-                        sendRequest(pool.get(0));
-                        pool.remove(0);
-                    }
-                }
-                try{
-                    Thread.sleep(30000);
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+
         edit_delayed_data = findViewById(R.id.edit_delayed_data);
         delayed_send_request = findViewById(R.id.delayed_send_request);
         delayed_result = findViewById(R.id.delayed_result);
@@ -59,6 +63,9 @@ public class DelayedActivity extends AppCompatActivity {
             public void onClick(View v) {
                 pool.add(edit_delayed_data.getText().toString());
                 edit_delayed_data.getText().clear();
+                if(pool.isEmpty()){
+                    handler.postDelayed(runnable, DELAY);
+                }
             }
         });
     }
@@ -71,7 +78,7 @@ public class DelayedActivity extends AppCompatActivity {
 
                 ArrayList<String> lines = responseParser(response);
 
-                final String serverResponse = lines.get(0) +"\n";
+                final String serverResponse = lines.get(0) + "\n";
 
                 runOnUiThread(new Runnable() {
                     @Override
@@ -81,13 +88,14 @@ public class DelayedActivity extends AppCompatActivity {
                         delayed_result.setText(tmp);
                     }
                 });
-
                 return true;
             }
         });
-        aRequest.postRequest(request,"http://sym.iict.ch/rest/txt", MediaType.parse("text/plain; charset=utf-8"));
+        aRequest.postRequest(request, "http://sym.iict.ch/rest/txt", MediaType.parse("text/plain; charset=utf-8"));
+
     }
-    public ArrayList responseParser(String response){
+
+    public ArrayList responseParser(String response) {
 
         ArrayList<String> parsedLines = new ArrayList<>();
         Scanner scanner = new Scanner(response);
@@ -102,4 +110,14 @@ public class DelayedActivity extends AppCompatActivity {
         return parsedLines;
 
     }
+
+    /**
+     * Check internet connections as seen in :
+     * https://stackoverflow.com/questions/1560788/how-to-check-internet-access-on-android-inetaddress-never-times-out
+     */
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnectedOrConnecting();
+    }
+
 }
